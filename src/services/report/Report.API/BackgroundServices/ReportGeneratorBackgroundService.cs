bc590aa2e1c93344bf83408a4f39492fef639efe
KÜRŞAD KOÇ <kursad.koc@saglik.gov.tr>
2022-11-21 10:48:48 +0300

    path update process after file generation completed

--- src/services/report/Report.API/BackgroundServices/ReportGeneratorBackgroundService.cs
+++ src/services/report/Report.API/BackgroundServices/ReportGeneratorBackgroundService.cs
@@ -9,6 +9,7 @@ using BuildingBlocks.Domain;
 using System.Text.Json;
 using Report.API.Infrastructure.Excel;
 using BuildingBlocks.Infrastructure.Serialization;
+using Report.API.RabbitMq.Events;
 
 namespace Report.API.BackgroundServices
 {
@@ -23,13 +24,16 @@ namespace Report.API.BackgroundServices
         private readonly RabbitMqConfiguration _rabbitMqConfiguration;
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly HttpClient _client;
+        private readonly ILogger<ReportGeneratorBackgroundService> _logger;
+        private IReportService _reportService;
 
         public ReportGeneratorBackgroundService(IServiceProvider serviceProvider,
                                                 ConnectionFactory connectionFactory,
                                                 RabbitMqConfiguration rabbitMqConfiguration,
                                                 IHttpClientFactory httpClientFactory,
                                                 IWebHostEnvironment env,
-                                                ISerializer serializer)
+                                                ISerializer serializer,
+                                                ILogger<ReportGeneratorBackgroundService> logger)
         {
             _serviceProvider = serviceProvider;
             _rabbitMqConfiguration = rabbitMqConfiguration;
@@ -38,10 +42,13 @@ namespace Report.API.BackgroundServices
             _client = _httpClientFactory.CreateClient("ContactApi");
             _env = env;
             _serializer = serializer;
+            _logger = logger;
         }
 
         public override Task StartAsync(CancellationToken cancellationToken)
         {
+            _logger.LogInformation("background service started...");
+
             _connection = _connectionFactory.CreateConnection();
             _channel = _connection.CreateModel();
 
@@ -74,36 +81,37 @@ namespace Report.API.BackgroundServices
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+            consumer.Received += Consumer_Received;
+            _channel.BasicConsume(queue: _rabbitMqConfiguration.QueueName, autoAck: false, consumer: consumer);
 
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var _reportService = scope.ServiceProvider.GetRequiredService<IReportService>();
-                var consumer = new AsyncEventingBasicConsumer(_channel);
-                consumer.Received += Consumer_Received;
-                _channel.BasicConsume(queue: _rabbitMqConfiguration.QueueName, autoAck: false, consumer: consumer);
-            }
 
             await Task.CompletedTask;
         }
 
         private async Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
         {
+            using var scope = _serviceProvider.CreateScope();
+            _reportService = scope.ServiceProvider.GetRequiredService<IReportService>();
+
             var message = Encoding.UTF8.GetString(@event.Body.ToArray());
+            RequestReportEvent requestReportEvent = _serializer.Deserialize<RequestReportEvent>(message);
+            await _reportService.RequestReport(requestReportEvent.Id, requestReportEvent.RequestDate, default);
             try
             {
                 string reportJson = await _client.GetStringAsync("/api/persons/GetReportData");
                 List<ReportItemDto> reportData = _serializer.Deserialize<List<ReportItemDto>>(reportJson);
                 var fileArray = ExcelHelper.GenerateExcel(reportData);
-                string filePath = Path.Combine(_env.ContentRootPath, "Reports",Guid.NewGuid() + ".xlsx");
+                string filePath = Path.Combine(_env.ContentRootPath, "Reports", Guid.NewGuid() + ".xlsx");
                 File.WriteAllBytes(filePath, fileArray);
-
+                await _reportService.SetReportPath(requestReportEvent.Id, filePath, default);
                 _channel.BasicAck(@event.DeliveryTag, false);
             }
             catch (Exception ex)
             {
                 _channel.BasicNack(@event.DeliveryTag, false, false);
+                _logger.LogError(ex.Message);
             }
-            await Task.CompletedTask;
         }
     }
 }
--- src/services/report/Report.API/RabbitMq/Events/RequestReportEvent.cs
+++ src/services/report/Report.API/RabbitMq/Events/RequestReportEvent.cs
@@ -2,9 +2,11 @@
 {
     public class RequestReportEvent
     {
+        public Guid Id { get; set; }
         public DateTime RequestDate { get; set; }
         public RequestReportEvent()
         {
+            Id = Guid.NewGuid();
             RequestDate = DateTime.UtcNow;
         }
     }
